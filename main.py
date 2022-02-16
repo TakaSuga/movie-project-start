84e1bf607d873c1a672a5d799ad5388988b5f5c8
TakaSuga <54337335+TakaSuga@users.noreply.github.com>
2022-02-16 17:51:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import requests
 
 # Create DB
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'secret_key'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie-collections.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 Bootstrap(app)
